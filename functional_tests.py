be54724be9f34a0dc6c3585714f8b8ad43fc8e0c
pathompat <mungonepiece@gmail.com>
2017-04-15 02:42:57 +0700

    First Commit

---
+++ functional_tests.py
@@ -0,0 +1,27 @@
+#Somchai heard about money management web app.. he want to try. first he go to it's homepage.
+
+#First thing he saw is title.The title name is "Money management".
+
+#He interest about "Manage your saving" and he try to click this.
+
+#He go to new url. This is "Saving Management"
+
+#He try to fill his income and outcome then he submit.
+
+#He saw history about his saving. an he come back to home page.
+
+#he look at the pie chart about his money.
+
+#Next,he found chart about gold price,stock price and Deposit interests.
+
+#He interest about gold price.then he go to see gold price history.
+
+#He think gold is a bad way to invest.then he come back.
+
+#Next,he go to stock price page.
+
+#That's good he think the stock is a best way to invest.he go back to home page.
+
+#He go to see bank interests it's not a way to rich.
+
+#He found the  best way he want to invest.
