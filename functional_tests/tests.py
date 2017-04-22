ef3419980369eccfa382f53931d29805c25fa6c4
pathompat <mungonepiece@gmail.com>
2017-04-22 21:50:32 +0700

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import unittest
 import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -11,10 +12,15 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('saving_table')
+        rows = table.find_elements_by_tag_name('td')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Somchai heard about money management web app.. he want to try. first
         #he go to it's homepage.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         #First thing he saw is title.The title name is "Money management".
         title_home = self.browser.find_element_by_tag_name('title').text
@@ -37,19 +43,27 @@ class NewVisitorTest(unittest.TestCase):
         inputbox1.send_keys('17000')
         inputbox2 = self.browser.find_element_by_id('id_des')
         inputbox2.send_keys('Money in banking')
-        #inputbox2.send_keys(Keys.ENTER)
         self.browser.find_element_by_id("id_sub").click()
         time.sleep(2)
 
+        #He try to fill his income 17000$ about 'Money in banking' then he submit.
+        sav_type = self.browser.find_element_by_id("id_type")
+        sav_type.click()
+        inputbox1 = self.browser.find_element_by_id('id_val')
+        inputbox1.send_keys('5000')
+        inputbox2 = self.browser.find_element_by_id('id_des')
+        inputbox2.send_keys('Won a lottery')
+        self.browser.find_element_by_id("id_sub").click()
+        time.sleep(5)
+
         #He saw history about his saving.
-        table = self.browser.find_element_by_id('saving_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('Money in banking', [row.text for row in rows])
+        self.check_for_row_in_list_table('Money in banking')
+        self.check_for_row_in_list_table('Won a lottery')
 
         #and he come back to home page.
         self.browser.find_element_by_partial_link_text('Back').click()
-        self.fail('Finish the test!')
         time.sleep(5)
+        self.fail('Finish the test!')
 
         #he look at the pie chart about his money.
 
